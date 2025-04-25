ec875dc5159d4ca45bd69aab466783add36156a9
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-25 20:30:37 +0300

    adding the last function

--- src/components/board.js
+++ src/components/board.js
@@ -46,9 +46,11 @@ function calculateWinner(squares) {
         [0, 3, 6], [1, 4, 7], [2, 5, 8],
         [0, 4, 8], [2, 4, 6],
       ];
-      
+
       for (let line of lines) {
         const [a, b, c] = line;
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
           return squares[a];
+        }
+        return null
 }
