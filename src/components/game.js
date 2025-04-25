c276d46b5000423553b02304c3eae2db3b9b0a1e
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-25 11:57:13 +0300

    winn formula

--- src/components/game.js
+++ src/components/game.js
@@ -4,7 +4,11 @@ const Game = () => {
             <h1>Tic Tac Toe</h1>
             <Board squares={board} onClick={handleClick} />
             <div className="game">
-
+            {winner ? (
+            <h2>Winner: {winner}</h2>
+             ) : (
+          <h2>Next Player: {isXNext ? 'X' : 'O'}</h2>
+            )}
             </div>
         </div>
     )
