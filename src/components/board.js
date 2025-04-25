06bf014fb1811c56b61d215a76484cce2e17ac21
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-25 20:27:23 +0300

    3rd board

--- src/components/board.js
+++ src/components/board.js
@@ -28,6 +28,9 @@ function Board () {
             <div className="board-row">
                 {renderSquare(3)}{renderSquare(4)}{renderSquare(5)}
             </div>
+            <div className="board-row">
+                {renderSquare(6)}{renderSquare(7)}{renderSquare(8)}
+            </div>
         </div>
     )
 }   
