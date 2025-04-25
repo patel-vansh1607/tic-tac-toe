cc25fca6f5f73ad0d411016180d695bac4cdfbfd
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-25 20:32:38 +0300

    adding fucntion

--- src/components/game.js
+++ src/components/game.js
@@ -1,17 +1,5 @@
-const Game = () => {
+function Game ({value, onclick}) {
     return(
-        <div className="main-div">
-            <h1>Tic Tac Toe</h1>
-            <Board squares={board} onClick={handleClick} />
-            <div className="game">
-            {winner ? (
-            <h2>Winner: {winner}</h2>
-             ) : (
-          <h2>Next Player: {isXNext ? 'X' : 'O'}</h2>
-            )}
-            </div>
-        </div>
+        
     )
 }
-
-export default Game
