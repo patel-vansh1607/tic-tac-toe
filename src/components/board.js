cb802dc064959d44d3f3e9f92e5fc42deb749b89
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-25 20:08:46 +0300

    winner function

--- src/components/board.js
+++ src/components/board.js
@@ -3,4 +3,6 @@ const { useState } = require("react");
 function Board () {
     const[squares, setSquares] = useState(Array(9).fill(null))
     const[isXNext, setIsXNext] = useState(true)
+
+    const winner = calculateWinner(squares)
 }
